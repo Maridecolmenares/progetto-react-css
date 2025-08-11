c473bc9a88f4bd026c95b1fc1e7818126a7959f7
Ivanna <iwanozka666@gmail.com>
2025-08-11 16:11:01 +0200

    feat: add ContactForm component with responsive layout and form state handling

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import CTABanner from './components/CTABanner/CTABanner'
+import CTABanner from './components/CTABanner/CTABanner';
 import  ImageTextBlock from './components/ImageTextBlock/ImageTextBlock';
-
+import ContactForm from './components/ContactForm/ContactForm';
 function App() {
   return (
     <>
       <CTABanner/>
       <ImageTextBlock />
+      <ContactForm/>
     </>
   )
 }
