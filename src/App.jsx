ff67f8652dfcc42c3025ed2887eb5195682e25f1
Ivanna <iwanozka666@gmail.com>
2025-08-12 17:22:27 +0200

    some chainges

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,6 @@ import  ImageTextBlock from './components/ImageTextBlock/ImageTextBlock';
 import Footer from './components/Footer/Footer'
 import ContactForm from './components/ContactForm/ContactForm';
 import Hero from './components/Hero/Hero';
-import CTABanner from './components/CTABanner/CTABanner'
-import ImageTextBlock from './components/ImageTextBlock/ImageTextBlock';
 import Header from './components/Header/Header';
 
 function App() {
