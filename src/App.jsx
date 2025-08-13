6d25141c7b9ec28faaae3ab55f8e08164fefe740
Ivanna <iwanozka666@gmail.com>
2025-08-13 18:47:46 +0200

    added the TextBlock component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import CTABanner from './components/CTABanner/CTABanner';
 import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import ContactForm from './components/ContactForm/ContactForm';
 import Hero from './components/Hero/Hero';
+import TextBlock from './components/TextBlock/TextBlock';
 import ImageBlock from './components/ImageBlock/ImageBlock';
 import ImageTextBlock from './components/ImageTextBlock/ImageTextBlock';
+import CTABanner from './components/CTABanner/CTABanner';
+import ContactForm from './components/ContactForm/ContactForm';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -16,11 +17,11 @@ function App() {
       <Header />
       <Hero/>
       <TextBlock />
-      <CTABanner />
-      <ImageTextBlock />
       <ImageBlock />
-      <Footer/>
+      <ImageTextBlock />
+      <CTABanner />
       <ContactForm/>
+      <Footer/>
     </>
   )
 }
